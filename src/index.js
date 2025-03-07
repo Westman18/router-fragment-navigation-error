3745a09911f5e79c8e6e7818d1335a0cb33d35bf
piotr.jarosz@wolterskluwer.com <piotr.jarosz@wolterskluwer.com>
2025-03-07 12:39:12 +0100

    prepare issue replication environment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Home = () => <h1>Home Page</h1>;
+const About = () => <h1>Mock Page</h1>;
+const NotFound = () => <h1>Page Not Found</h1>;
+
+// pre-defined router is needed as a dependency for application initializer
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+  },
+]);
+
+// initailize application with router...
+
+// routesDefinitions are returned from application initializer
+const routesDefinitions = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/mock",
+    element: <About />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
+
+router.patchRoutes("root", routesDefinitions);
+
+// resolve full browser url on init / page refresh, AFTER pathRoutes has finished
+await router.navigate(
+  window.location.pathname + window.location.search + window.location.hash,
+  {
+    replace: true,
+  }
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
